caa3b208c3d61d788f980228f1911e129aa50e8d
Andrey Dantas <andreydantasvf@gmail.com>
2024-09-23 17:45:01 -0300

    fix: home image

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -6,9 +6,7 @@ export function HomePage() {
   return (
     <div className="bg-background_black h-[90vh] md:h-[85vh] flex items-center justify-center md:pt-32">
       <div className="max-w-[1440px] mt-20 md:mt-0 px-12 md:px-0 flex flex-col md:flex-row-reverse md:gap-16 justify-center md:justify-between items-center space-y-8">
-        <div className="w-60 md:w-[32rem] h-52 md:h-[21rem] bg-slate-500 rounded-full">
-          <Image alt="Foto de exemplo" src="/exemplo_barbeiro.png" width={513} height={340} />
-        </div>
+        <Image alt="Foto de exemplo" src="/exemplo_barbeiro.png" width={513} height={340} quality={100} />
 
         <div className="flex flex-col items-start">
           <h1 className="border-b border-gold">
