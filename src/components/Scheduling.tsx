aa1d6bd9b2e940fbc764d4719f2efaed49456172
Andrey Dantas <andreydantasvf@gmail.com>
2024-09-24 15:24:05 -0300

    feat: add services prices

--- src/components/Scheduling.tsx
+++ src/components/Scheduling.tsx
@@ -6,15 +6,23 @@ import { RxScissors } from "react-icons/rx";
 import { GiBeard, GiToothbrush } from "react-icons/gi";
 import { useState } from "react";
 import { FaWhatsapp } from "react-icons/fa";
+import { PiPaintBrushHouseholdFill } from "react-icons/pi";
+import { GiRazor } from "react-icons/gi";
 
 const servicesList = [
-  { value: "cabelo", label: "Cabelo", icon: RxScissors },
-  { value: "barba", label: "Barba", icon: GiBeard },
-  { value: "selagem", label: "Selagem", icon: GiToothbrush },
+  { value: "cabelo", label: "Cabelo", icon: RxScissors, price: 20 },
+  { value: "barba", label: "Barba", icon: GiBeard, price: 10 },
+  { value: "selagem", label: "Selagem", icon: GiToothbrush, price: 60 },
+  { value: "luzes", label: "Luzes", icon: PiPaintBrushHouseholdFill, price: 80 },
+  { value: "sobrancelha", label: "Sobrancelha", icon: GiRazor, price: 5 },
 ];
 
 export function Scheduling() {
   const [selectedServices, setSelectedServices] = useState<string[]>(["cabelo"]);
+
+  const totalValue = servicesList
+    .filter(service => selectedServices.includes(service.value))
+    .reduce((acc, service) => acc + service.price, 0);
 
   return (
     <section id="agendamento" className="bg-background_light flex items-center flex-col justify-center py-10">
@@ -28,12 +36,12 @@ export function Scheduling() {
         </div>
 
         <div className="w-full flex items-center justify-center">
-          <div className="w-72 min-h-96 md:w-[34rem] md:h-[39rem] bg-white border border-[9B9EA3] rounded-md mt-10 relative flex flex-col justify-between">
+          <div className="w-72 md:w-[34rem] pb-5 md:pb-14 bg-white border border-[9B9EA3] rounded-md mt-10 relative flex flex-col">
             <div className="absolute -left-3 top-7">
-              <Image className="h-[21rem] md:h-auto" src="/ideia-caderno.svg" alt="lateral de caderno" width={24} height={572} quality={100} />
+              <Image className="h-[21rem] md:h-[31rem] w-6" src="/ideia-caderno.svg" alt="lateral de caderno"  width='0' height='0' sizes="100vw" quality={100} />
             </div>
 
-            <form action="" className="text-sm md:text-base mx-7 md:mx-14 my-5 md:my-11 font-barlow-semi-condensed flex flex-col items-center gap-3 md:gap-6">
+            <form action="" className="text-sm md:text-base mx-7 md:mx-14 mt-5 md:mt-11 font-barlow-semi-condensed flex flex-col items-center gap-4 md:gap-6">
               <div className="w-full space-y-1 md:space-y-2">
                 <label htmlFor="name" className="text-[5F6368] font-bold">Nome</label>
                 <input className="border border-A19D9D bg-[#F9F9F9] w-full p-2 md:p-4 rounded-md" type="text" name="name" id="name" placeholder="Digite o seu Nome" />
@@ -54,10 +62,12 @@ export function Scheduling() {
                   variant="destructive"
                   maxCount={3}
                 />
+
+                <span className="text-[#5F6368] font-barlow-semi-condensed font-bold text-base inline-block mt-2">Total: R$ {totalValue}</span>
               </div>
             </form>
 
-            <div className="flex justify-center items-center mb-12">
+            <div className="flex justify-center items-center mt-10 md:mt-12">
               <button className="max-h-14 rounded-md font-roboto-slab bg-gold text-[#373535] font-bold p-3 md:p-4 text-xs flex items-center gap-2 md:text-base hover:bg-[#F9B256] transition-all ease-in-out duration-300">SOLICITAR AGENDAMENTO <FaWhatsapp className="mb-1" size={20} /></button>
             </div>
           </div>
--- src/components/ui/multi-select.tsx
+++ src/components/ui/multi-select.tsx
@@ -244,7 +244,7 @@ export const MultiSelect = React.forwardRef<
                       )}
                       style={{ animationDuration: `${animation}s` }}
                     >
-                      {`+ ${selectedValues.length - maxCount} more`}
+                      {`+ ${selectedValues.length - maxCount} serviços`}
                       <XCircle
                         className="ml-2 h-4 w-4 cursor-pointer"
                         onClick={(event) => {
